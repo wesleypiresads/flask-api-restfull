41884b0610006803f18da102037b6a256bdef6de
Wesley Pires <wesleypiresads@gmail.com>
2020-04-07 00:01:05 -0300

    Criado método GET

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 
+app = Flask(__name__)
 
+desenvolvedores = [
+    {
+        'nome': 'Wesley Pires',
+        'habilidades': ['Python', 'Flask']
+    },
+    {
+        'nome': 'Rafael Cordas',
+        'habilidades': ['Javascript', 'HTML']
+    },
+    {
+        'nome': 'Ana Silva',
+        'habilidades': ['PHP', 'HTML', 'CSS']
+    }
+]
+
+@app.route('/dev')
+def desenvolvedor():
+    desenvolvedor = desenvolvedores
+    return jsonify(desenvolvedor)
+
+if __name__ == "__main__":
+    app.run(debug=True)
